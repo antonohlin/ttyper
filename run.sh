eab47e9e7f3d9fac59a419d90ad10ab99c90a2a0
Tobias Laross <Tobias.laross@assaabloy.com>
2025-12-02 08:55:44 +0100

    Added symlink to run.sh on build (#50)

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+script_path="$0"
+while [ -L "$script_path" ]; do
+    link_target="$(readlink "$script_path")"
+    case "$link_target" in
+    /*) script_path="$link_target" ;;
+    *) script_path="$(dirname "$script_path")/$link_target" ;;
+    esac
+done
+script_path="$(cd "$(dirname "$script_path")" && pwd)/$(basename "$script_path")"
+script_directory="$(dirname "$script_path")"
+
 build=0
 
 show_help() {
@@ -23,7 +34,20 @@ while getopts ":h?b" opt; do
 done
 
 if [ $build -eq 1 ]; then
-  ./gradlew uberJar --console=plain || exit 1
+    ./gradlew uberJar --console=plain || exit 1
+
+    INSTALL_DIR="$HOME/.local/bin"
+
+    case ":$PATH:" in
+    *":$INSTALL_DIR:"*) ;;
+    *)
+        echo "Warning: $INSTALL_DIR is not in your PATH. You won't be able to run 'ttyper' globally."
+        ;;
+    esac
+
+    mkdir -p "$INSTALL_DIR"
+    ln -sf "$script_directory/run.sh" "$INSTALL_DIR/ttyper"
+    echo "Installed symlink: $INSTALL_DIR/ttyper"
 fi
 
-java --enable-native-access=ALL-UNNAMED -jar build/libs/ttyper.jar "$@"
+java --enable-native-access=ALL-UNNAMED -jar "$script_directory/build/libs/ttyper.jar" "$@"
